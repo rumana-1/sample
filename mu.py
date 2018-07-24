02ab25fd1bb63f55ef82a61ebf5d3033b005cee0
rumana-1 <40631009+rumana-1@users.noreply.github.com>
2018-07-24 12:14:22 +0530

    Add files via upload

---
+++ mu.py
@@ -0,0 +1,8 @@
+import pygame
+import time
+
+pygame.mixer.init()
+
+pygame.mixer.music.load('C:\\Users\\Anjum.DESKTOP-26UF8UT\\Desktop\\ide nadu ide bhashe MP3.mp3 ')
+pygame.mixer.music.play()
+time.sleep(30)
